c963561c3f193471dc693089eed60b436cb79702
AnnaGosme <anna.gosme@gmail.com>
2024-04-25 01:26:14 +0200

    add cursors, contact form, result form, error form, modify skills planets colors and position

---
+++ reasons.php
@@ -0,0 +1,8 @@
+<?php
+
+$reasons = [
+    "wisdomAndGuidance" => "Sagesse et Conseil",
+    "spiritualAndPhilosophicalInsights" => "Perspective Spirituelle et Philosophique",
+    "mentorshipAndLearning" => "Mentorat et Apprentissage",
+    "uniquePerspectiveAndHumor" => "Perspective Unique et Humour",
+];
