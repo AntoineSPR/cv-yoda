831879885ab9bc6d4de72d981bd7d8d399aeb5f6
AntoineSPR <antoine.simper@gmail.com>
2024-04-25 18:02:13 +0200

    Added condition to execute script on mobile only

--- menu.js
+++ menu.js
@@ -1,23 +1,30 @@
 let menuLink = document.querySelectorAll("nav a");
 let checkbox = document.getElementById("checkbox-menu");
 
-checkbox.addEventListener("change", () => {
-    if (checkbox.checked) {
-        menuLink.forEach(link => {
-            link.style.right = "0px";
-        });
-    } else {
-        menuLink.forEach(link => {
-            link.style.right = "-330px";
-        });
-    }
-});
+function isMobile() {
+    return window.matchMedia('(max-width: 768px)').matches;
+}
 
-menuLink.forEach(link => {
-    link.addEventListener("click", () => {
-        checkbox.checked = false;
-        menuLink.forEach(link => {
-            link.style.right = "-330px";
+if (isMobile()) {
+    checkbox.addEventListener("change", () => {
+        if (checkbox.checked) {
+            menuLink.forEach(link => {
+                link.style.right = "0px";
+            });
+        } else {
+            menuLink.forEach(link => {
+                link.style.right = "-330px";
+            });
+        }
+    });
+    
+    menuLink.forEach(link => {
+        link.addEventListener("click", () => {
+            checkbox.checked = false;
+            menuLink.forEach(link => {
+                link.style.right = "-330px";
+            });
         });
     });
-});
+}
+
