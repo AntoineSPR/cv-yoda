109a788b169e322f76490f428937d7a04cb5163f
AntoineSPR <antoine.simper@gmail.com>
2024-04-25 16:59:17 +0200

    Corrected script for the menu

--- menu.js
+++ menu.js
@@ -1,8 +1,19 @@
 let menuLink = document.querySelectorAll("nav a");
 let checkbox = document.getElementById("checkbox-menu");
 
+checkbox.addEventListener("change", () => {
+    if (checkbox.checked) {
+        menuLink.forEach(link => {
+            link.style.right = "0px";
+        });
+    }
+});
+
 menuLink.forEach(link => {
     link.addEventListener("click", () => {
         checkbox.checked = false;
+        menuLink.forEach(link => {
+            link.style.right = "-330px";
+        });
     });
 });
