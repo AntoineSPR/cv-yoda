9af455dfb42803831b7350722dc2fdad8ee1fabc
AnnaGosme <anna.gosme@gmail.com>
2024-04-25 22:36:23 +0200

    add favicon

--- webroot/error.php
+++ webroot/error.php
@@ -4,6 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" href="media/jedi-order.png">
   <link rel="stylesheet" href="styles/error.css" />
   <title>Erreur, il y a</title>
 </head>
