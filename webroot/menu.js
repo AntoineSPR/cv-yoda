45f081643889abc1c2d4c7512c493ed36dabf537
AntoineSPR <antoine.simper@gmail.com>
2024-04-27 12:47:50 +0200

    Updated script for dynamic menu

--- webroot/menu.js
+++ webroot/menu.js
@@ -2,7 +2,7 @@ let menuLink = document.querySelectorAll("nav a");
 let checkbox = document.getElementById("checkbox-menu");
 let overlay = document.getElementById("overlay");
 
-// Check if the screen is in mobile mode (warning: only checks once when the DOM is loaded)
+// Check if the screen is in mobile/tablet mode
 function isMobile() {
     return window.matchMedia('(max-width: 1279px)').matches;
 }
@@ -18,21 +18,34 @@ function collapse(element) {
     });
 }
 
-if (isMobile()) {
-    checkbox.addEventListener("change", () => {
-        if (checkbox.checked) {
-            menuLink.forEach(link => {
-                link.style.right = "0px";
-            });
-            overlay.style.display = "block";
-        } else {
-            menuLink.forEach(link => {
-                link.style.right = "-330px";
-            });
-            overlay.style.display = "none";
-        }
-    });
-    
-    menuLink.forEach(link => collapse(link));
-    collapse(overlay);
+// Dynamically handle button movement, overlay and menu collapse
+function animateButtons() {
+    if (isMobile()) {
+        checkbox.addEventListener("change", () => {
+            if (checkbox.checked) {
+                menuLink.forEach(link => {
+                    link.style.right = "0px";
+                });
+                overlay.style.display = "block";
+            } else {
+                menuLink.forEach(link => {
+                    link.style.right = "-330px";
+                });
+                overlay.style.display = "none";
+            }
+        });
+
+        menuLink.forEach(link => collapse(link));
+        collapse(overlay);
+    } else {
+        menuLink.forEach(link => {
+            link.style.right = '';
+        });
+    }
 }
+
+animateButtons();
+
+window.addEventListener('resize', () => {
+    animateButtons();
+}); 
